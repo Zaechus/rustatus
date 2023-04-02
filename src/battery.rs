8234cdfea9ce9274dd81816047608198420a8af0
Zaechus <zaechus@pm.me>
2023-04-02 10:54:23 -0600

    match battery status on not charging

--- src/battery.rs
+++ src/battery.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use crate::colored_block;
+use crate::{block, colored_block};
 
 pub fn battery_block() -> String {
     if let Ok(capacity) = fs::read_to_string("/sys/class/power_supply/BAT0/capacity") {
@@ -8,11 +8,13 @@ pub fn battery_block() -> String {
 
         let (icon, color) = match (capacity, status()) {
             (_, "Charging") => ('\u{f1e6}', ""),
-            (90.., _) => ('\u{f240}', ""),
-            (66.., _) => ('\u{f241}', ""),
-            (33.., _) => ('\u{f242}', ""),
-            (10.., _) => ('\u{f243}', ""),
-            (0.., _) => ('\u{f244}', "#cc241d"),
+            (95.., "Not charging") => return block(" \u{f240}  "),
+            (90.., "Discharging") => ('\u{f240}', ""),
+            (66.., "Discharging") => ('\u{f241}', ""),
+            (33.., "Discharging") => ('\u{f242}', ""),
+            (10.., "Discharging") => ('\u{f243}', ""),
+            (0.., "Discharging") => ('\u{f244}', "#cc241d"),
+            _ => ('?', ""),
         };
 
         colored_block(&format!(" {icon}  {capacity}%"), color)
@@ -27,6 +29,8 @@ fn status<'a>() -> &'a str {
         .trim()
     {
         "Charging" => "Charging",
+        "Discharging" => "Discharging",
+        "Not charging" => "Not charging",
         _ => "",
     }
 }
