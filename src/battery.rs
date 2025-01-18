651b964fd551eca9ff92122f508681d1b8cc5ff5
Zaechus <zaechus@pm.me>
2025-01-18 14:49:42 -0700

    try both BAT0 and BAT1

--- src/battery.rs
+++ src/battery.rs
@@ -3,28 +3,33 @@ use std::fs;
 use crate::{block, colored_block, RED};
 
 pub fn battery_block() -> String {
-    if let Ok(capacity) = fs::read_to_string("/sys/class/power_supply/BAT0/capacity") {
-        let capacity = capacity.trim().parse::<u8>().unwrap();
+    for bat in ["BAT0", "BAT1"] {
+        if let Ok(capacity) =
+            fs::read_to_string(format!("/sys/class/power_supply/{}/capacity", bat))
+        {
+            let capacity = capacity.trim().parse::<u8>().unwrap();
 
-        let icon = match (capacity, status()) {
-            (_, "Charging") => '\u{f1e6}',
-            (94.., "Full" | "Not charging") => return block("\u{f240} "),
-            (90.., "Discharging") => '\u{f240}',
-            (66.., "Discharging") => '\u{f241}',
-            (33.., "Discharging") => '\u{f242}',
-            (10.., "Discharging") => '\u{f243}',
-            (0.., "Discharging") => return colored_block(&format!("\u{f244}  {capacity}%"), RED),
-            _ => '?',
-        };
+            let icon = match (capacity, status(bat)) {
+                (_, "Charging") => '\u{f1e6}',
+                (94.., "Full" | "Not charging") => return block("\u{f240} "),
+                (90.., "Discharging") => '\u{f240}',
+                (66.., "Discharging") => '\u{f241}',
+                (33.., "Discharging") => '\u{f242}',
+                (10.., "Discharging") => '\u{f243}',
+                (0.., "Discharging") => {
+                    return colored_block(&format!("\u{f244}  {capacity}%"), RED)
+                }
+                _ => '?',
+            };
 
-        block(&format!("{icon}  {capacity}%"))
-    } else {
-        String::new()
+            return block(&format!("{icon}  {capacity}%"));
+        }
     }
+    String::new()
 }
 
-fn status<'a>() -> &'a str {
-    match fs::read_to_string("/sys/class/power_supply/BAT0/status")
+fn status<'a>(bat: &str) -> &'a str {
+    match fs::read_to_string(format!("/sys/class/power_supply/{}/status", bat))
         .unwrap()
         .trim()
     {
