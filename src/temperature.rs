61a758179c683e52c0634e123fc04b7cc390ee1c
Zaechus <zaechus@pm.me>
2026-03-27 02:33:11 -0600

    empty temp block if no temps

--- src/temperature.rs
+++ src/temperature.rs
@@ -3,27 +3,31 @@ use std::fs;
 use crate::{block, colored_block, RED};
 
 pub fn temperature_block() -> String {
-    let temperature = temperature();
-
-    if temperature > 80 {
-        colored_block(&format!("{}°C", temperature), RED)
+    if let Some(temperature) = temperature() {
+        if temperature > 80 {
+            colored_block(&format!("{}°C", temperature), RED)
+        } else {
+            block(&format!("{}°C", temperature))
+        }
     } else {
-        block(&format!("{}°C", temperature))
+        String::new()
     }
 }
 
-fn temperature() -> u32 {
-    fs::read_dir("/sys/class/thermal")
-        .unwrap()
-        .map(|x| x.unwrap().file_name().into_string().unwrap())
-        .filter(|x| x.starts_with("thermal_zone"))
-        .map(|x| {
-            if let Ok(s) = fs::read_to_string(format!("/sys/class/thermal/{}/temp", x)) {
-                s.trim().parse::<u32>().unwrap_or_default() / 1000
-            } else {
-                0
-            }
-        })
-        .max()
-        .unwrap()
+fn temperature() -> Option<u32> {
+    if let Ok(thermal_dir) = fs::read_dir("/sys/class/thermal") {
+        thermal_dir
+            .map(|x| x.unwrap().file_name().into_string().unwrap())
+            .filter(|x| x.starts_with("thermal_zone"))
+            .map(|x| {
+                if let Ok(s) = fs::read_to_string(format!("/sys/class/thermal/{}/temp", x)) {
+                    s.trim().parse::<u32>().unwrap_or_default() / 1000
+                } else {
+                    0
+                }
+            })
+            .max()
+    } else {
+        None
+    }
 }
